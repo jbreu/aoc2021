1f754f6c701253c8389e82e20d4c89c969f81d49
jbreu <jakobbreu@googlemail.com>
2021-12-04 10:28:55 +0100

    achieved 4th day, 2nd task

--- day4.py
+++ day4.py
@@ -1,5 +1,3 @@
-import sys
-
 with open('input4') as f:
     first = True
     tables = []
@@ -20,17 +18,29 @@ with open('input4') as f:
     tables.append(current_table)
     tables.pop(0)
 
+cleared = []
+
 for draw in draws:
     for i, table in enumerate(tables):
+        if i in cleared:
+            continue
+
         table = [[0 if cell == draw else cell for cell in row] for row in table]
         tables[i] = table
 
         summed_rows = [sum(row) for row in table]
 
-        #transpose
+        # transpose
         table = list(map(list, zip(*table)))
         summed_columns = [sum(row) for row in table]
 
         if any(row == 0 for row in summed_rows) or any(column == 0 for column in summed_columns):
-            print(sum(summed_rows) * draw)
-            sys.exit()
+            # Task 7
+            if not cleared:
+                print(sum(summed_rows) * draw)
+
+            # Task 8
+            if len(tables) - len(cleared) == 1:
+                print(sum(summed_rows) * draw)
+
+            cleared.append(i)
