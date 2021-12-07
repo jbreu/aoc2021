2d84080ce12e211837ce814b4275422c29e00aef
Jakob Breu <jakobbreu@googlemail.com>
2021-12-07 09:39:10 +0100

    working 7th day 2nd task

--- day7.py
+++ day7.py
@@ -1,4 +1,5 @@
 import statistics
+import math
 
 with open('input7') as f:
     crabs = [line.rstrip().split(",") for line in f][0]
@@ -21,7 +22,9 @@ def gauss_sum(num):
     return int((num * num + num) / 2)
 
 
-low_diff = sum(gauss_sum(abs(c - low)) for c in crabs)
-high_diff = sum(gauss_sum(abs(c - high)) for c in crabs)
+best_so_far = math.inf
 
-print(min(low_diff, high_diff))
+for candidate in range(min(crabs), max(crabs) + 1):
+    best_so_far = min(best_so_far, sum(gauss_sum(abs(c - candidate)) for c in crabs))
+
+print(best_so_far)
