f2449e575777b2ac9eedb8ea16c7fae7b71cecda
jbreu <jakobbreu@googlemail.com>
2021-12-10 13:48:14 +0100

    day10 full solved

--- day10.py
+++ day10.py
@@ -1,5 +1,8 @@
+import statistics
+
 with open('input10') as f:
     chunks = [line.rstrip() for line in f]
+
 
 def check(chunk):
       
@@ -14,19 +17,40 @@ def check(chunk):
         elif i in close_brackets:
             if not queue or i != queue.pop():
                 return i
+
+    score = 0
+
+    while len(queue)>0:
+        score *= 5
+
+        char = queue.pop()
+
+        if char == ')':
+            score+=1
+        if char == ']':
+            score+=2
+        if char == '}':
+            score+=3
+        if char == '>':
+            score+=4
+
+    scores.append(score)
+    
    
 illegal = 0
+scores = []
 
 for chunk in chunks:
     result = check(chunk)
 
     if result == ')':
         illegal+=3
-    if result == ']':
-        illegal+=57
-    if result == '}':
-        illegal+=1197
-    if result == '>':
+    elif result == '>':
         illegal+=25137
-
+    elif result == ']':
+        illegal+=57
+    elif result == '}':
+        illegal+=1197
 print(illegal)
+
+print(statistics.median(scores))
